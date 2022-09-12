5cf6d2152774cf5565117c00affb0d3ce5c8e477
MonkyDonkey <113388389+MonkyDonkey@users.noreply.github.com>
2022-09-13 00:13:23 +0300

    Create createJoinedDomainVMScript.ps1

---
+++ createJoinedDomainVMScript.ps1
@@ -0,0 +1,19 @@
+Connect-AzurreRmAccount
+$resourceGroupName = "newJoinedDomainVM"
+$resourceGroupLocation = "eastus"
+$rg = New-AzureRmResourceGroup -Name $resourceGroupName - Location $resourceGroupLocation
+
+$parameters = @{
+  existingVnetName = "adVNET";
+  existingSubnetName = "adSubnet";
+  domainToJoin = "exam.com";
+  domainUsername = "admin";
+  domainPassword = "youDontKnowMyPassword";
+  ouPath = "OU=testOU,DC=exam,DC=com";
+  adminUsername = "vmAdmin";
+  adminPassword = "vmPassword"
+  
+}
+$templatePath = https://github.com/MonkyDonkey/Azure/raw/main/createVmAndAddToDomain.json
+
+New-AzureRmResourceGroupDeployment -ResourceGroupname $rg.ResourceGroupname -TemplateFile $templatePath $parameters
